8b7aa511c9136182bc141be5fa91add6bb1701b1
shegy28 <shegy28@gmail.com>
2023-01-16 19:40:28 +0100

    Add localstorage using life cycle

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -6,23 +6,7 @@ import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
     state = {
-        todos: [
-          {
-            id: uuidv4(),
-            title: "Setup development environment",
-            completed: true
-          },
-          {
-            id: uuidv4(),
-            title: "Develop website and add content",
-            completed: false
-          },
-          {
-            id: uuidv4(),
-            title: "Deploy to live server",
-            completed: false
-          }
-        ]
+        todos: [],
     };
 
     handleChange = id => {
@@ -60,6 +44,38 @@ class TodoContainer extends React.Component {
         });
     };
 
+    setUpdate = (updatedTitle, id) => {
+        this.setState({
+          todos: this.state.todos.map(todo => {
+            if (todo.id === id) {
+              todo.title = updatedTitle
+            }
+            return todo
+          }),
+        })
+    }
+
+    componentDidMount() {
+        const temp = localStorage.getItem("todos")
+        const loadedTodos = JSON.parse(temp)
+        if (loadedTodos) {
+          this.setState({
+            todos: loadedTodos
+          })
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.todos !== this.state.todos) {
+          const temp = JSON.stringify(this.state.todos)
+          localStorage.setItem("todos", temp)
+        }
+    }
+
+    componentWillUnmount() {
+        console.log("Cleaning up...")
+    }
+
     render() {
         return (
           <div className="container">
@@ -70,6 +86,7 @@ class TodoContainer extends React.Component {
                 todos={this.state.todos}
                 handleChangeProps={this.handleChange}
                 deleteTodoProps={this.delTodo}
+                setUpdate={this.setUpdate}
               />
             </div>
           </div>
