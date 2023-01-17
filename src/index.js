678fc4102a243eba93097afba0d7bcfed16b071b
shegy28 <shegy28@gmail.com>
2023-01-17 10:27:06 +0100

    Add linter checks and fixed errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import TodoContainer from "./functionBased/components/TodoContainer"
-import "./functionBased/App.css"
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import TodoContainer from './functionBased/components/TodoContainer';
+import './functionBased/App.css';
 
 // eslint-disable-next-line
 ReactDOM.render(
-    <React.StrictMode>
-      <Router>
-        <TodoContainer />
-      </Router>
-    </React.StrictMode>,
-    document.getElementById('root'),
-  );
-
+  <React.StrictMode>
+    <Router>
+      <TodoContainer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
